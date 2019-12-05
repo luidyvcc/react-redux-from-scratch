60120d4bedf5f63843072c3123c434bf2db5c791
Luidy Vaccaro <luidy@upoutsourcing.com>
2019-12-05 15:15:32 -0300

    lesson 35 done

---
+++ 003-projeto-calculadora/calculadora/src/components/Button.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Button.css';
+
+
+
+export default props =>
+    <button className="button">{props.label}</button>
---
+++ 003-projeto-calculadora/calculadora/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './main/Calculator';
+
+ReactDOM.render(<Calculator />, document.getElementById('root'));
