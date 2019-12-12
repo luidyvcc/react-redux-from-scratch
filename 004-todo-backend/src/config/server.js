f7831097b192bdad2bc85e239dba8169573566d6
Luidy Vaccaro <luidy@upoutsourcing.com>
2019-12-12 14:50:23 -0300

    lesson 48 done

--- 004-todo-backend/src/config/server.js
+++ 004-todo-backend/src/config/server.js
@@ -3,10 +3,12 @@ const port = 3001;
 const bodyParser = require('body-parser');
 const express = require("express");
 const server = express();
+const allowCors = require("./cors");
 
 // middleware:
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(allowCors);
 
 server.listen(port, function () {
     console.log(`BACKEND is running on port ${port}.`)
